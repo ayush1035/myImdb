525a39e4f22ff9e1e7e1232f373568e2c583fd4f
Ayush Jain <30284809+ayush1035@users.noreply.github.com>
2018-02-23 09:38:57 +0530

    fixed html

--- src/components/static/staticPresentational.js
+++ src/components/static/staticPresentational.js
@@ -6,9 +6,6 @@ class Static extends React.Component {
             return(
                 <div>
                     <h1 className="heading">Please select a movie to show details</h1>
-                    <div className="imdbLogo">
-                        <img src="https://vignette.wikia.nocookie.net/eastereggs/images/f/f8/IMDB_Logo.png/revision/latest?cb=20160930213814" alt="imdb"/>
-                    </div>
                 </div>
             )
        
